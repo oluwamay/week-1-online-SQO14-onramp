f167de9cb1318d52297d3e712ad3f9b475ce4837
oluwamay <samuelmayowao060@gmail.com>
2022-11-02 22:35:29 +0100

    another algo updte

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,7 +1,14 @@
 function fizzBuzz(input){
-
-  
-}
+    if(input % 3 == 0 && input % 5 == 0){
+        return "fizzbuzz"
+    }else if(input % 5 == 0){
+        return "buzz"
+    }else if(input % 3 == 0 ){
+        return "fizz"
+    }else{
+        return "not divisible by 3 or 5"
+    }
+    }
 
 
 module.exports = fizzBuzz
