9f417891ff277eef1b6c2b1aaa50fcf6cccad798
oluwamay <samuelmayowao060@gmail.com>
2022-11-12 09:12:45 +0100

    updated algorithms

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,10 @@
 function countTruthy(arr) {
-
+   let falsy = [undefined, null, NaN, 0, "", false]
+    count = 0
+   for(let i = 0; i < arr.length; i++){
+        if(!falsy.includes(arr[i]))
+            count++
+   }
+   return count
 }
 module.exports = countTruthy
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,14 +1,17 @@
 function fizzBuzz(input){
     if(input % 3 == 0 && input % 5 == 0){
-        return "fizzbuzz"
+        return "fizzBuzz"
     }else if(input % 5 == 0){
         return "buzz"
     }else if(input % 3 == 0 ){
         return "fizz"
     }else{
-        return "not divisible by 3 or 5"
+        return input
     }
-    }
-
-
+}
+//Implement a function fizzBuzz(input) that takes a number and retuns:
+// "fizz": if the number is divisible by 3
+// "buzz": if the number is divisible by 5
+// "fizzbuzz": if the number is both divisible by 3 and 5
+// number: if it is not divisible by 3 or 5
 module.exports = fizzBuzz
